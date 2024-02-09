8b80b8e046b368412a962e76da0714d5fb21ec04
albertojbe <141800257+albertojbe@users.noreply.github.com>
2024-02-09 11:32:43 -0300

    Create MediaPonderada.java

---
+++ Codigos em Java/MediaPonderada.java
@@ -0,0 +1,29 @@
+package Java;
+import java.util.Scanner;
+
+public class MediaPonderada{
+    public static void main(String[] args){
+        float n1, n2, n3, p1, p2, p3, media;
+
+        System.out.println("Média Ponderada");
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nDigite três notas e seus respectivos pesos");
+        System.out.print("\nNota 1: ");
+        n1 = scan.nextInt();
+        System.out.print("Seu peso: ");
+        p1 = scan.nextInt();
+        System.out.print("\nNota 2: ");
+        n2 = scan.nextInt();
+        System.out.print("Seu peso: ");
+        p2 = scan.nextInt();
+        System.out.print("\nNota 3: ");
+        n3 = scan.nextInt();
+        System.out.print("Seu peso: ");
+        p3 = scan.nextInt();
+        
+         media = ((n1*p1) + (n2*p2) + (n3*p3)) / (p1+p2+p3);
+         System.out.printf("\n\nA média ponderada é: %f", media);
+    
+    }
+}
